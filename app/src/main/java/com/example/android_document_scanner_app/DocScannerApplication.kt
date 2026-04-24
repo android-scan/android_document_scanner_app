52eeefcacd9fc7bb07a06d9cc1c827f3402f6acf
MrHouston <mrhoustonvr@yandex.ru>
2026-04-25 01:32:13 +0300

    chore: configure Application class and manifest for scanner app

---
+++ app/src/main/java/com/example/android_document_scanner_app/DocScannerApplication.kt
@@ -0,0 +1,7 @@
+package com.example.android_document_scanner_app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DocScannerApplication : Application()
