c9dbd8e9a2145c3e15a2156ae69721e08ab22525
Artyom Rashevsky <albatroz3000@yandex.ru>
2026-04-28 21:21:26 +0300

    add ONNX detector

--- app/src/main/java/com/example/android_document_scanner_app/di/CvModule.kt
+++ app/src/main/java/com/example/android_document_scanner_app/di/CvModule.kt
@@ -1,11 +1,13 @@
 package com.example.android_document_scanner_app.di
 
+import android.content.Context
 import com.example.android_document_scanner_app.cv.CornerDetector
 import com.example.android_document_scanner_app.cv.ImageProcessor
-import com.example.android_document_scanner_app.cv.OpenCVCornerDetector
+import com.example.android_document_scanner_app.cv.OnnxCornerDetector
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -15,10 +17,8 @@ object CvModule {
 
     @Provides
     @Singleton
-    fun provideCornerDetector(): CornerDetector {
-        // ДС ЧАСТЬ: заменить на OnnxCornerDetector(context) когда получим model.onnx
-        return OpenCVCornerDetector()
-    }
+    fun provideCornerDetector(@ApplicationContext context: Context): CornerDetector =
+        OnnxCornerDetector(context)
 
     @Provides
     @Singleton
